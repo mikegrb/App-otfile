0bf035f6948bfa36406d04f883a8888934b28430
mikegrb <mgreb@linode.com>
2013-09-09 01:56:46 -0400

    cpanify

---
+++ cpanfile
@@ -0,0 +1,6 @@
+requires 'perl', '5.010001';
+recommends 'Term::Progressbar';
+recommends 'Mac::Pasteboard';
+recommends 'File::MMagic';
+recommends 'UUID::Tiny';
+recommends 'URI::Escape';
---
+++ lib/App/otfile.pm
@@ -0,0 +1,23 @@
+package App::otfile;
+ 
+use strict;
+use vars qw($VERSION);
+$VERSION = '0.03';
+ 
+1;
+ 
+__END__
+ 
+=head1 NAME
+ 
+App::otfile - Serve a single file, once, via HTTP over the local network.
+ 
+=head1 DESCRIPTION
+ 
+See L<otfile>. The module L<App::otfile> is just a placeholder for CPAN.
+ 
+=head1 SEE ALSO
+ 
+L<otfile>.
+ 
+=cut
